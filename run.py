1aef4d7d62cb35a72c0bfc52251f59a0e9cb2670
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-02-08 20:21:10 +0000

    Feat: Add inital description print messages.

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print('--------------------------------------------------------------------')
+print('Welcome to the Waiting List system for the 1st Dublin Scout Group.')
+print('--------------------------------------------------------------------\n')
+print('OPTIONS:')
+print('1. Add my child to the waiting list.')
+print('2. Check my child\'s position on the waiting list.')
+print('3. ADMIN ONLY - Edit waiting list.')
+choice = input('Enter a number from the options above and press enter:\n')
+print(f'You chose option {choice}')
