95dfbdf320ed8d502c73d488fd04e30decf9b55b
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-02-08 21:32:36 +0000

    Feat: Add register_details function with initial print statements.

--- run.py
+++ run.py
@@ -13,15 +13,42 @@ def get_user_choice():
         choice = input('Enter a number from the options above and press '
                        'enter:\n')
         if choice == '1' or choice == '2' or choice == '3':
-            print(f'You chose option {choice}')
+            #print(f'You chose option {choice}')
             break
         else:
             print(f'\nINVALID INPUT: "{choice}". You must enter a '
                   'number between 1 and 3.\n')
     return choice
 
+def register_details():
+    '''
+    Gather personal details from user.
+    User details and child details will be recorded in a new row
+    in the spreadsheet and given an unique reference number.
+    '''
+
+    print('--------------------------------------------')
+    print('You chose: Add my child to the waiting list.')
+    print('--------------------------------------------\n')
+    fname = input('Please enter your first name: ')
+    lname = input('Please enter your last name: ')
+    email = input('Please enter your email address: ')
+    cfname = input('Please enter your child\'s first name: ')
+    clname = input('Please enter your child\'s last name: ')
+    dob = input('Please enter your child\'s date of birth in the format '
+                'DD/MM/YYYY: ')
+    print(f'Your details are as follows:\n'
+          f'Full Name: {fname} {lname}\n'
+          f'Contact email: {email}\n'
+          f'Child\'s Full Name: {cfname} {clname}\n'
+          f'Child\'s DOB: : {dob}\n'
+          'Are these details correct? (y/n)\n'
+          )
+
 print('------------------------------------------------------------------')
 print('Welcome to the Waiting List system for the 1st Dublin Scout Group.')
 print('------------------------------------------------------------------\n')
 
-get_user_choice()
+choice = get_user_choice()
+
+register_details() if choice == '1' else print(f'Choice: {choice}')
