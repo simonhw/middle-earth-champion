d616761b7b4d567384189743cdb973858a569c56
Simon Henleywillis <simonhenleywillis@gmail.com>
2024-02-17 18:47:42 +0000

    Fix: Add \n to API error messages.

--- run.py
+++ run.py
@@ -252,7 +252,7 @@ def push_details(list):
 
     worksheet = list[-1]
     try:
-        SHEET.worksheet(worksheets).append_row(list)
+        SHEET.worksheet(worksheet).append_row(list)
         print(f'Thank you, {list[3]} has been added to the {worksheet} '
                'waiting list!')
         print('Your reference is: ' + list[-2])
@@ -261,7 +261,7 @@ def push_details(list):
         print(f'We will be in touch when we have capacity for {list[3]} to join.\n')
     except:
         print('We\'re sorry, there was a problem accessing the database. '
-              'Please try again later.')
+              'Please try again later.\n')
 
 
 def get_details():
